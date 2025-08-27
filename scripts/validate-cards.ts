8de85d5b28cbe65c694313f23eb430bd2dca3d7e
Kyle J. Kemp <kyle@seiyria.com>
2025-08-27 10:35:58 -0500

    attempt to add github annotations

--- scripts/validate-cards.ts
+++ scripts/validate-cards.ts
@@ -8,31 +8,31 @@ const allIdsByLocale = {};
 
 allCards.forEach((card, index) => {
   if (!card.id) {
-    console.error(`Card ${index} does not have an id.`);
+    console.error(`::error:: Card ${index} does not have an id.`);
     process.exit(1);
   }
 
   if (!card.locale) {
-    console.error(`Card ${card.id} does not have a locale.`);
+    console.error(`::error:: Card ${card.id} does not have a locale.`);
     process.exit(1);
   }
 
   allIdsByLocale[card.locale] = allIdsByLocale[card.locale] || {};
 
   if (allIdsByLocale[card.locale][card.id]) {
-    console.error(`Card ${card.id} has a duplicate id.`);
+    console.error(`::error:: Card ${card.id} has a duplicate id.`);
     process.exit(1);
   }
 
   allIdsByLocale[card.locale][card.id] = true;
 
   if (!card.name) {
-    console.error(`Card ${card.id} does not have a name.`);
+    console.error(`::error:: Card ${card.id} does not have a name.`);
     process.exit(1);
   }
 
   if (!card.image) {
-    console.error(`Card ${card.id} does not have an image.`);
+    console.error(`::error:: Card ${card.id} does not have an image.`);
     process.exit(1);
   }
 
@@ -42,25 +42,25 @@ allCards.forEach((card, index) => {
   }/${fixedImage}.png`;
   if (!fs.existsSync(imagePath)) {
     console.error(
-      `Card ${card.id} references an invalid image "${imagePath}".`,
+      `::error:: Card ${card.id} references an invalid image "${imagePath}".`,
     );
     process.exit(1);
   }
 
   if (!card.game) {
-    console.error(`Card ${card.id} does not have a game.`);
+    console.error(`::error:: Card ${card.id} does not have a game.`);
     process.exit(1);
   }
 
   if (!card.tags || card.tags.length === 0) {
-    console.error(`Card ${card.id} does not have any tags.`);
+    console.error(`::error:: Card ${card.id} does not have any tags.`);
     process.exit(1);
   }
 
   if (card.flipSide) {
     const flipSide = allCards.find((c) => c.id === card.flipSide);
     if (!flipSide) {
-      console.error(`Card ${card.id} has invalid flipSide ${card.flipSide}`);
+      console.error(`::error:: Card ${card.id} has invalid flipSide ${card.flipSide}`);
       process.exit(1);
     }
   }
@@ -81,7 +81,7 @@ allCards.forEach((card, index) => {
 
   if (Object.keys(otherProps).length > 0) {
     console.error(
-      `Card ${card.id} has other props: ${Object.keys(otherProps).join(',')}`,
+      `::error:: Card ${card.id} has other props: ${Object.keys(otherProps).join(',')}`,
     );
     process.exit(1);
   }
